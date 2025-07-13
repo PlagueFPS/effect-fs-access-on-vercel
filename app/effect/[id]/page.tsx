20737887fddcaa5fb7733986aecdd9ac41aed6a2
Plague <74983533+PlagueFPS@users.noreply.github.com>
2025-07-13 11:04:28 -0400

    add dynamic metadata to try and get the generation to work

--- app/effect/[id]/page.tsx
+++ app/effect/[id]/page.tsx
@@ -1,3 +1,23 @@
+import type { Metadata } from "next"
+
+export const generateMetadata = async ({ params }: { params: Promise<{ id: string }> }): Promise<Metadata> => {
+  const { id } = await params
+  return {
+    title: `Dynamic Effect Page: ${id}`,
+    description: 'This is a dynamic page using Effect FS access on Vercel.',
+    openGraph: {
+      title: `Dynamic Effect Page: ${id}`,
+      description: 'This is a dynamic page using Effect FS access on Vercel.',
+      type: "website",
+    },
+    twitter: {
+      card: "summary_large_image",
+      title: `Dynamic Effect Page: ${id}`,
+      description: 'This is a dynamic page using Effect FS access on Vercel.',
+    }
+  }
+}
+
 export default async function EffectDynamicPage({ params }: { params: Promise<{ id: string }> }) {
   const { id } = await params
 
